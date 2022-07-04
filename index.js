aec9eec7fd2d4f4e09a601cc5fb9b07211d9d99f
tialantzy <tialantzy@gmail.com>
2022-07-04 10:48:39 -0500

    commit tree 002231afaf7fc334909be0a9f47a0f85c2215c03

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (blocks) {
+    if (blocks > 42) {
+    return blocks - 42; 
+} else {
+    return 42 - blocks; 
+}
+}
+
+function distanceFromHqInFeet (blocks) {
+    return distanceFromHqInBlocks (blocks) * 264
+
+}
+
+function distanceTravelledInFeet (beginning, end) {
+    if (beginning < end) {
+        return (end - beginning) * 264; 
+    } else {
+        return (beginning - end) * 264
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+const distance = distanceTravelledInFeet (start, destination);
+
+if (distance < 400) {
+    return 0;
+} else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400); 
+} else if (distance > 2000 && distance < 2500) {
+    return 25; 
+} else {
+    return 'cannot travel that far'
+}
+}
